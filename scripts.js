f524cef09e7bce0400b340ab52f1763bb9b16b38
ZK <kongzhikeng@gmail.com>
2023-03-07 23:36:00 -0500

    third commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 let photoList = new Array();
-photoList[0]="url(../public/images/IMG_6073.jpg)";
-photoList[1]="url(../public/images/IMG_7010.jpg)";
+photoList[0]="url(../public/images/auto_1.jpeg)";
+photoList[1]="url(../public/images/auto_2.jpeg)";
+photoList[2]="url(../public/images/auto_3.jpeg)";
+photoList[3]="url(../public/images/auto_4.jpeg)";
+photoList[4]="url(../public/images/auto_5.jpeg)";
+photoList[5]="url(../public/images/auto_6.jpeg)";
+
 let autoPlay = document.getElementById("auto_photo");
 let x =-1;
 let l = photoList.length;
