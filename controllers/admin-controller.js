89a89e72732c64925d86d6d479f3055572d0fdfb
ZK <kongzhikeng@gmail.com>
2023-06-04 17:40:13 -0400

    adminCtrl update

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -5,55 +5,63 @@ const Perso = require('../models/perso-model')
 
 module.exports = {
     admin_res: async function (request, response) {
-        if(request.isAuthenticated()){
-        await Response.find().then(function (allRes) {
-            response.render('pages/adminRes', {
-                responseArray: allRes,
-                // response.send("This route points to the Admin Console page")
+        if (request.isAuthenticated()) {
+            await Response.find().then(function (allRes) {
+                response.render('pages/adminRes', {
+                    responseArray: allRes,
+                    // response.send("This route points to the Admin Console page")
+                })
+            }).catch(function (error) {
+                console.log(error)
             })
-        }).catch(function (error) {
-            console.log(error)
-        })
-    }
+        }
     },
     create_res: (request, response) => {
-        response.render('pages/createRes')
-        // response.send("This route points to the Create page")
+        if (request.isAuthenticated()) {
+            response.render('pages/createRes')
+            // response.send("This route points to the Create page")
+        }
     },
     update_res: async function (request, response) {
-        const { _id } = request.params;
-        // const foundRes = data.find(res => res._id === String(_id));
-        await Response.findOne({ _id: _id }).then(function (foundRes) {
-            response.render('pages/updateRes', {
-                foundRes: foundRes
-            });
-        }).catch(function (error) {
-            console.log(error)
-        })
+        if (request.isAuthenticated()) {
+            const { _id } = request.params;
+            // const foundRes = data.find(res => res._id === String(_id));
+            await Response.findOne({ _id: _id }).then(function (foundRes) {
+                response.render('pages/updateRes', {
+                    foundRes: foundRes
+                });
+            }).catch(function (error) {
+                console.log(error)
+            })
+        }
         // response.send("This route points to the Update page")
     },
     admin_perso: async function (request, response) {
-        if(request.isAuthenticated()){
-        await Perso.find().then(function (allPerso) {
-            response.render('pages/adminPerso', {
-                persoArray: allPerso,
+        if (request.isAuthenticated()) {
+            await Perso.find().then(function (allPerso) {
+                response.render('pages/adminPerso', {
+                    persoArray: allPerso,
+                })
+            }).catch(function (error) {
+                console.log(error)
             })
-        }).catch(function (error) {
-            console.log(error)
-        })
-    }
+        }
     },
     create_perso: (request, response) => {
-        response.render('pages/createPerso')
+        if (request.isAuthenticated()) {
+            response.render('pages/createPerso')
+        }
     },
     update_perso: async function (request, response) {
-        const { _id } = request.params;
-        await Perso.findOne({ _id: _id }).then(function (foundPerso) {
-            response.render('pages/updatePerso', {
-                foundPerso: foundPerso
-            });
-        }).catch(function (error) {
-            console.log(error)
-        })
+        if (request.isAuthenticated()) {
+            const { _id } = request.params;
+            await Perso.findOne({ _id: _id }).then(function (foundPerso) {
+                response.render('pages/updatePerso', {
+                    foundPerso: foundPerso
+                });
+            }).catch(function (error) {
+                console.log(error)
+            })
+        }
     }
 }
